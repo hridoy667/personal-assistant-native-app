96aba5c584040767a810ef5101c090bd2092a217
Ali Imam Hridoy <ali.imamhref@gmail.com>
2026-08-14 23:08:20 +0600

    first commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { ThemeProvider, DefaultTheme, DarkTheme } from '@react-navigation/native';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 
 import { AnimatedSplashOverlay } from '@/components/animated-icon';
 import AppTabs from '@/components/app-tabs';
+import '../global.css'; // Make sure global Tailwind CSS is imported here!
 
 SplashScreen.preventAutoHideAsync();
 
